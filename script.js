6cf78d3f5ec1de623b9ff1df0a459f6063245fd7
JASPREET KAUR <94101161+jasbhatia@users.noreply.github.com>
2023-05-04 18:05:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 
-var yourArray=[ "abc", 2, true, false, "one"];
-// console.log(yourArray);
-return yourArray;
+// var yourArray=[ "abc", 2, true, false, "one"];
+// // console.log(yourArray);
+// return yourArray;
+let yourArray = [];
+for(let i = 0; i < 5; i++){
+	let obj = {`str${i + 1}`, i, true};
+	let obj = {`str&{i + 1}`, i, true};
+	yourArray.push(obj);
+}
+console.log(yourArray.length);
